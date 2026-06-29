5d238e0d129a9c64d905dd9c65a1f189dd565c9a
saventsv <saventv@gmail.com>
2026-06-29 10:22:43 -0500

    add the function definition for remove client to client.h

--- include/client.h
+++ include/client.h
@@ -5,5 +5,6 @@ Client *get_client_win(Workspace *ws, Window win);
 void swap_clients(Workspace *ws, int idx1, int idx2);
 void append_client(Workspace *ws, Client *client);
 void insert_client(Workspace *ws, Client *client, int idx);
+void remove_client(Workspace *ws, int idx);
 ClientArray init_client_arr();
 
