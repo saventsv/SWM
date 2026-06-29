d571738ed4b8a730eb45508fba455595df3c8e95
saventsv <saventv@gmail.com>
2026-06-29 07:48:46 -0500

    changed to storing clients in a array rather than a linked list

--- rewrite.c
+++ rewrite.c
@@ -1,38 +1,53 @@
 #include <X11/X.h>
-#include <X11/Xatom.h>
 #include <X11/Xlib.h>
+#include <X11/Xatom.h>
 #include <X11/Xutil.h>
+#include <X11/extensions/Xrandr.h>
 #include <X11/keysym.h>
+#include <stdlib.h>
+
 
 #define MAX_WORKSPACES 9
 
-typedef struct Client {
+typedef struct {
   Window window;
   int x, y, width, height;
-  struct Client *next_client;
 } Client;
 
+
 typedef struct {
-  Client *master_client;
+  Client *data;
+  size_t capacity;
+  size_t size;
+} ClientArray;
+
+typedef struct {
+  ClientArray clients;
   int focused_idx;
   int n_clients;
   int id;
 } Workspace;
 
-typedef struct {
-  Workspace workspaces[MAX_WORKSPACES];
-  int current_workspace;
-  int running;
-  Display *dpy;
-} WindowManager;
-
 typedef struct Monitor{
   int monitor_width, monitor_height;
+  int monitor_x, monitor_y;
+  // eventually used for handling reserving space for status bars
   int usable_width, usable_height;
+  int usable_x, usable_y;
   Workspace *ws;
   // eventual multi-monitor support
   // struct Monitor *next
 } Monitor;
+
+typedef struct {
+  Workspace workspaces[MAX_WORKSPACES];
+  int current_workspace;
+  int running;
+  Monitor *monitors;
+  int n_monitors;
+  Display *dpy;
+} WindowManager;
+
 
 void handle_maprequest(XEvent event);
 void handle_configurerequest(XEvent event);
@@ -40,12 +55,11 @@ void handle_destorynotify(XEvent event);
 void tile(Display *dpy, Workspace *ws);
 void calculate_geometry(Workspace *ws);
 WindowManager init_wm();
-int get_client_idx(Workspace *ws, Client *client);
-Client *get_client(Workspace *ws, int idx);
 Client *get_client_win(Workspace *ws, Window win);
 void swap_clients(Workspace *ws, int idx1, int idx2);
 void append_client(Workspace *ws, Client *client);
 void insert_client(Workspace *ws, Client *client, int idx);
+ClientArray init_client_arr();
 
 
 WindowManager init_wm() {
@@ -54,14 +68,40 @@ WindowManager init_wm() {
   wm.current_workspace = 0;
   wm.running = 1;
 
+  if(!(wm.dpy = XOpenDisplay(0x0))) abort();
+
   for(int i = 0; i < MAX_WORKSPACES; i++) {
     Workspace workspace;
     workspace.focused_idx = -1;
-    workspace.master_client = NULL;
+    workspace.clients = init_client_arr();
     workspace.n_clients = 0;
     workspace.id = i;
 
     wm.workspaces[i] = workspace;
+  }
+
+  int n_monitors;
+
+  XRRMonitorInfo *monitors = XRRGetMonitors(
+      wm.dpy, 
+      DefaultRootWindow(wm.dpy), 
+      True,
+      &n_monitors);
+
+  wm.n_monitors = n_monitors;
+
+  wm.monitors = malloc(n_monitors * sizeof(Monitor));
+
+  if(wm.monitors == NULL) abort();
+
+  for(int i = 0; i < n_monitors; i++) {
+    Monitor monitor;
+    monitor.monitor_x = monitors[i].x;
+    monitor.monitor_y = monitors[i].y;
+    monitor.monitor_width = monitors[i].width;
+    monitor.monitor_height = monitors[i].height;
+
+    wm.monitors[i] = monitor;
   }
 
   return wm;
@@ -75,7 +115,6 @@ int main(void) {
 
   XEvent event;
 
-  if(!(WM.dpy = XOpenDisplay(0x0))) return 1;
 
 
 
