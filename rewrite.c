fa722a53760249c5dcb92ec9640bc3b3c8273951
saventsv <saventv@gmail.com>
2026-06-28 20:30:22 -0500

    add basic needed types

---
+++ rewrite.c
@@ -0,0 +1,28 @@
+#include <X11/X.h>
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/keysym.h>
+
+#define MAX_WORKSPACES 9
+
+typedef struct Client {
+  Window window;
+  int x, y, width, height;
+  struct Client *next_client;
+} Client;
+
+typedef struct {
+  Client *master_client;
+  int focused_idx;
+  int n_clients;
+  int id;
+} Workspace;
+
+typedef struct {
+  Workspace workspaces[MAX_WORKSPACES];
+  int current_workspace;
+  int running;
+} WindowManager;
+
+
