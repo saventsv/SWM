175c855acc4aeced6e826248f7cdef1f32df70d6
saventsv <saventv@gmail.com>
2026-06-28 20:14:36 -0500

    removed the gitignore file

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A development flake for a x11 window manager";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  ouputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        libx11
+      ];
+    };
+  };
+}
