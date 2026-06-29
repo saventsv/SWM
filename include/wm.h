15ff92bfa5e41264792db42529f53140e220464c
saventsv <saventv@gmail.com>
2026-06-29 18:13:36 -0500

    comment out the focused_monitor field due to wanting to add support for multi monitors later

--- include/wm.h
+++ include/wm.h
@@ -38,7 +38,7 @@ typedef struct {
   Workspace workspaces[MAX_WORKSPACES];
   int current_workspace;
   int running;
-  int focused_monitor;
+  // int focused_monitor;
   Monitor *monitors;
   int n_monitors;
   Display *dpy;
