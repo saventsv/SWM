9d4dcd73ae54b221415027a3e3f7df1629bed127
saventsv <saventv@gmail.com>
2026-06-29 07:53:24 -0500

    implement init_client_arr function

--- rewrite.c
+++ rewrite.c
@@ -107,6 +107,18 @@ WindowManager init_wm() {
   return wm;
 }
 
+ClientArray init_client_arr() {
+  ClientArray client_arr;
+  client_arr.capacity = 4;
+  client_arr.size = 0;
+
+  client_arr.data = malloc(client_arr.capacity * sizeof(Client));
+
+  if(client_arr.data == NULL) abort();
+
+  return client_arr;
+}
+
 
 
 int main(void) {
