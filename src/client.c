3762214702dc1e8606613c81f30da194d4039039
saventsv <saventv@gmail.com>
2026-06-29 10:19:13 -0500

    implemented the insert_client function

--- src/client.c
+++ src/client.c
@@ -28,9 +28,34 @@ void append_client(Workspace *ws, Client *client) {
 void swap_clients(Workspace *ws, int idx1, int idx2) {
   if(idx1 < 0 || idx1 > ws->clients.size - 1) return;
   if(idx2 < 0 || idx2 > ws->clients.size - 1) return;
+  if(idx2 == idx1) return;
 
   Client *client = ws->clients.data[idx1];
 
   ws->clients.data[idx1] = ws->clients.data[idx2];
   ws->clients.data[idx2] = client;
+}
+
+void insert_client(Workspace *ws, Client *client, int idx) {
+  if(ws->clients.size == ws->clients.capacity) {
+    ws->clients.data = realloc(ws->clients.data, ws->clients.capacity * 2 * sizeof(Client*));
+
+    if(ws->clients.data == NULL) abort();
+
+    ws->clients.capacity*=2;
+  } 
+
+  if(idx == ws->clients.size) {
+    append_client(ws, client);
+    return;
+  }
+
+  for(int i = ws->clients.size; i > idx; --i) {
+    // Not using the swap_clients due to it only being allowed to swap non null indicies
+    Client *client = ws->clients.data[i];
+    ws->clients.data[i] = ws->clients.data[i - 1];
+    ws->clients.data[i - 1] = client;
+  }
+
+  ws->clients.data[idx] = client;
 }
---
+++ tests/insert_client.c
@@ -0,0 +1,58 @@
+#include "../include/client.h"
+#include <assert.h>
+#include <stdio.h>
+
+int main(void) {
+  Workspace ws;
+  ws.clients = init_client_arr();
+
+  Client c1, c2, c3, c4, c5;
+
+  append_client(&ws, &c1);
+  append_client(&ws, &c2);
+  append_client(&ws, &c3);
+  append_client(&ws, &c4);
+
+  insert_client(&ws, &c5, 0);
+
+  assert(ws.clients.data[0] == &c5);
+  assert(ws.clients.data[1] == &c1);
+  assert(ws.clients.data[2] == &c2);
+  assert(ws.clients.data[3] == &c3);
+  assert(ws.clients.data[4] == &c4);
+
+  for(int i = 0; i < ws.clients.size; i++) {
+    ws.clients.data[i] = NULL;
+  }
+  ws.clients.size = 0;
+
+  append_client(&ws, &c1);
+  append_client(&ws, &c2);
+  append_client(&ws, &c3);
+  append_client(&ws, &c4);
+
+  insert_client(&ws, &c5, 4);
+  assert(ws.clients.data[4] == &c5);
+
+  for(int i = 0; i < ws.clients.size; i++) {
+    ws.clients.data[i] = NULL;
+  }
+  ws.clients.size = 0;
+
+  append_client(&ws, &c1);
+  append_client(&ws, &c2);
+  append_client(&ws, &c3);
+  append_client(&ws, &c4);
+
+  insert_client(&ws, &c5, 3);
+
+  assert(ws.clients.data[0] == &c1);
+  assert(ws.clients.data[1] == &c2);
+  assert(ws.clients.data[2] == &c3);
+  assert(ws.clients.data[3] == &c5);
+  assert(ws.clients.data[4] == &c4);
+
+  return 0;
+}
+
+
